aaa00cd10d65fd4227206579ea41c5c7d241f25d
Raphaël <raphael_asm@hotmail.fr>
2016-06-13 14:00:55 +0200

    systeme de récupération de mot de passe(2)

--- app/Views/user/forgotpasswordmodif.php
+++ app/Views/user/forgotpasswordmodif.php
@@ -5,11 +5,11 @@
 
     <label for="new_password1">Nouveau mot de passe</label>
     <input required type="password" name="new_password1" value="<?php if (!empty($newPassword1)){echo  $newPassword1;} ?>">
-    <span class="error"><?php if (!empty($error['newPassword1'])){ echo  $error['newPassword1']; } ?></span>
+    <span class="error"><?php if (!empty($error['new_password1'])){ echo  $error['new_password1']; } ?></span>
 
     <label for="new_password2">Répeter le nouveau mot de passe</label>
     <input required type="password" name="new_password2" value="<?php if (!empty($newPassword2)){echo  $newPassword2;} ?>">
-    <span class="error"><?php if (!empty($error['newPassword2'])){ echo  $error['newPassword2']; } ?></span>
+    <span class="error"><?php if (!empty($error['new_password2'])){ echo  $error['new_password2']; } ?></span>
     <input type="submit">
 
 </form>
