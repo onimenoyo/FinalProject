e969ef041bce6656fb5b21aa1ab309a5ea4cc5cf
Raphaël <raphael_asm@hotmail.fr>
2016-06-20 09:35:01 +0200

    changement pour inscription izi

--- app/Views/user/sent_mail.php
+++ app/Views/user/sent_mail.php
@@ -2,9 +2,5 @@
 
 <?php $this->start('main_content');?>
 <h3>Un e-mail à été envoyer à l'adresse suivante : <?= $email ?></h3>
-<h3>Pour rendre actif mail: </h3>
 <?php
-?><h3>Pour récup mot de passe :</h3>
-<?php
-  echo '<a href="http://localhost/FinalProject/public/forgotpassword/'.$token.'"> http://localhost/FinalProject/public/forgotpassword/'.$token.'</a>';
  $this->stop('main_content') ?>
